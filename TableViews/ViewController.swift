c53f61465d8927a7ae3db76a3ce628f6dc788c78
ricardo0441 <ricardo0441@gmail.com>
2023-10-11 12:37:42 -0600

    init
    CoreData project initialization for TableView

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Ricardo Rodriguez.
+//  Copyright © 2023 RicardoDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let myCountries = ["España", "Mexico", "Perú", "Colombia", "Argentina", "EEUU", "Francia", "Italia"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        
+    }
+
+    @IBAction func add(_ sender: Any) {
+        print("Añadir Datos")
+    }
+    
+}
+
+
+
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountries.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+            
+            var cell = tableView.dequeueReusableCell(withIdentifier: "mycell")
+            if cell == nil {
+               
+                cell = UITableViewCell(style: .default, reuseIdentifier: "mycell")
+                cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+                
+            }
+            cell!.textLabel?.text = myCountries[indexPath.row]
+            return cell!
+      
+            
+       
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        print(myCountries[indexPath.row])
+    }
+    
+}
+
